a8afb354b90f4ab46ceecac8c2c1f8bc90f27dc1
IlonaKuzmina <ikuzmina88@gmail.com>
2023-03-06 15:25:56 +0200

    .

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,29 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `test`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    "gatsby-plugin-sass",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `PirktAuto`,
+        short_name: `PirktAuto`,
+        start_url: `/`,
+        theme_color: `#DCB47A`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
+    },
+  ],
 };
 
 export default config;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { Link } from "gatsby";
-// import { StaticImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 
 const Header = () => {
     return (
         <div className="container">
             <Link to="/">
-                {/* <StaticImage className="header_logo" src="../images/Pirktauto_logo_horizontal_black.png" alt="logo" /> */}
+                <StaticImage className="header_logo" src="../images/Pirktauto_logo_horizontal_black.png" alt="logo" />
             </Link>
             <Link to="/">Home</Link>
             <Link to="/shop">Shop</Link>
