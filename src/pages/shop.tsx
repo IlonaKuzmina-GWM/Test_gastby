02eb2783d1b0db6d7ae55ace9bdf1b6c8162367d
IlonaKuzmina <ikuzmina88@gmail.com>
2023-03-08 12:39:41 +0200

    shop page 2%

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import HomeAutoCard from "../components/HomeAutoCard";
 import MainLayout from "../layouts/MainLayout";
 
 const ShopPage = () => {
     return (
         <MainLayout>
-            <div className="container">
-                <h1>Shop Page</h1>
+            <div className="shop-page-container">
+                <div className="filters-container">
+                    <p>filter side</p>
+                </div>
+                <div className="auto-cards-container">
+                    <HomeAutoCard imageUrl={"../images/Escultures.png"} title={"Good auto"} price={1500} labels={<><div>Label1</div><div>label2</div></>}></HomeAutoCard>
+                    <HomeAutoCard imageUrl={"../images/Escultures.png"} title={"Good auto"} price={1500} labels={<><div>Label1</div><div>label2</div></>}></HomeAutoCard>
+                    <HomeAutoCard imageUrl={"../images/Escultures.png"} title={"Good auto"} price={1500} labels={<><div>Label1</div><div>label2</div></>}></HomeAutoCard>
+                </div>
             </div>
         </MainLayout>
     );
