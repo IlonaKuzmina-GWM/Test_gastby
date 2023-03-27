adfc157a0a9dfa6d7401d278b2155e0f3e21f4b0
IlonaKuzmina <ikuzmina88@gmail.com>
2023-03-27 10:20:15 +0300

    ?

--- src/components/ShopAutoCard.tsx
+++ src/components/ShopAutoCard.tsx
@@ -14,7 +14,7 @@ type ShopAutoCardProps = {
 const ShopAutoCard: FC<ShopAutoCardProps> = ({ title, price, handleClick, imageUrl }) => {
     return (
         <Card border="light" className="shop-auto-card px-0 m-2 mb-3">
-            <Nav.Link href="/single">
+            <Nav.Link href="/">
             {/* <Card.Img variant="top" src={imageUrl} /> */}
             {/* <Card.Img variant="top" src={"../images/Escultures.png"}></Card.Img> */}
             <StaticImage className="shop-card-image" src={"../images/Escultures1.png"} alt={"Auto"} />
