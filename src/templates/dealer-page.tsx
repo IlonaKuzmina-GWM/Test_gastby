025fc5f8430c2cc5d5bf0879ed06cff3e8598e9a
IlonaKuzmina <ikuzmina88@gmail.com>
2023-05-25 17:05:56 +0300

    build Error handling

--- src/templates/dealer-page.tsx
+++ src/templates/dealer-page.tsx
@@ -3,34 +3,35 @@ import MainLayout from "../layouts/MainLayout";
 import { graphql, useStaticQuery } from "gatsby";
 
 type DealerPageProps = {
-    pageContext: {
-        databaseId: number; // Add the databaseId property
-      };}
+    // pageContext: {
+    //     databaseId: number; // Add the databaseId property
+    //   };
+}
 
-const DealerPage: FC<DealerPageProps> = ({ pageContext }) => {
-    const filteredByDealerName = useStaticQuery(graphql`
-    query CarsByDealerQuery($databaseId: Int!) {
-      allWpCar(filter: { carCategories: { nodes: { elemMatch: { databaseId: { eq: $databaseId } } } } }) {
-        nodes {
-          title
-          slug
-          carInfo {
-            carPrice
-          }
-          featuredImage {
-            node {
-              gatsbyImage(cropFocus: CENTER, fit: COVER, formats: WEBP, placeholder: BLURRED, width: 1024, height: 1024)
-            }
-          }
-        }
-      }
-    }
-  `);
+const DealerPage: FC<DealerPageProps> = ({ }) => {
+//     const filteredByDealerName = useStaticQuery(graphql`
+//     query CarsByDealerQuery($databaseId: Int!) {
+//       allWpCar(filter: { carCategories: { nodes: { elemMatch: { databaseId: { eq: $databaseId } } } } }) {
+//         nodes {
+//           title
+//           slug
+//           carInfo {
+//             carPrice
+//           }
+//           featuredImage {
+//             node {
+//               gatsbyImage(cropFocus: CENTER, fit: COVER, formats: WEBP, placeholder: BLURRED, width: 1024, height: 1024)
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `);
 
 
     // const dealerId = pageContext
 
-    console.log(filteredByDealerName)
+    // console.log(filteredByDealerName)
 
     return (
         <MainLayout>
