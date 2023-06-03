f1053d369630a4f27fdc51a838c15143d93f1c98
IlonaKuzmina <ikuzmina88@gmail.com>
2023-06-03 12:42:22 +0300

    .

--- src/html.js
+++ src/html.js
@@ -28,7 +28,7 @@ export default function HTML(props) {
         <link rel="stylesheet" href="/node_modules/owl.carousel/dist/assets/owl.carousel.min.css" />
         <link rel="stylesheet" href="/node_modules/owl.carousel/dist/assets/owl.theme.default.css" />
         <link rel="stylesheet" href="/bower_components/owl.carousel/dist/assets/owl.carousel.min.css" /> */}
-        <link
+        {/* <link
           rel="preload"
           href="/fonts/WorkSans-Regular.woff2"
           as="font"
@@ -55,7 +55,7 @@ export default function HTML(props) {
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
-        ></link>
+        ></link> */}
 
         {/* Use maximum-scale and user-scalable at your own risk. It disables pinch/zoom. Think about usability/accessibility before including. */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0"/>
