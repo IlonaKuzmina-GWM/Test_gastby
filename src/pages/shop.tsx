8eda03772abc879fabb9956c20277e2ffa6916c0
IlonaKuzmina <ikuzmina88@gmail.com>
2023-08-09 10:01:02 +0300

    .

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -173,11 +173,11 @@ const ShopPage: FC<ShopProps> = ({ location, data }) => {
                         }
                     </Row>
 
-                    <Row xs={12} className="g-4 mt-5">
+                    {/* <Row xs={12} className="g-4 mt-5">
                         {filteredCars.length < 1 || carsToRender.length < 1 && <div className="d-flex justify-content-center align-items-center">
                             <h3 className="text-center">Diemžēl pēc izvēlētiem kriterijiem nekas nav atrasts</h3>
                         </div>}
-                    </Row>
+                    </Row> */}
                 </Container>
             </div>
         </MainLayout>
