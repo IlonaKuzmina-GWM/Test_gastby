7b1db3d7772a39edd88381de26175044f924a14f
IlonaKuzmina <ikuzmina88@gmail.com>
2023-10-19 13:53:32 +0300

    .

--- src/html.js
+++ src/html.js
@@ -16,35 +16,35 @@ export default function HTML(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0" />
         <link
           rel="preload"
-          href="../static/fonts/Poppins-Regular.woff2"
+          href="/fonts/Poppins-Regular.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
         />
         <link
           rel="preload"
-          href="../static/fonts/Poppins-Bold.woff2"
+          href="/fonts/Poppins-Bold.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
         />
         <link
           rel="preload"
-          href="../static/fonts/Poppins-Light.woff2"
+          href="/fonts/Poppins-Light.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
         />
         <link
           rel="preload"
-          href="../static/fonts/Poppins-Medium.woff2"
+          href="/fonts/Poppins-Medium.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
         />
         <link
           rel="preload"
-          href="../static/fonts/Poppins-SemiBold.woff2"
+          href="/fonts/Poppins-SemiBold.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
