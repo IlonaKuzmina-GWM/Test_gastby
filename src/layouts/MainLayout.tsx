39326def275c0c01badcd55d13e1c84a5ba3cd53
IlonaKuzmina <ikuzmina88@gmail.com>
2023-10-19 13:48:15 +0300

    .

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,6 @@ import React, { FC, ReactNode } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "../scss/index.scss";
-import "../scss/core/core.scss";
 
 type MainLayoutProps = {
     children?: ReactNode;
@@ -11,13 +10,6 @@ type MainLayoutProps = {
 const MainLayout: FC<MainLayoutProps> = ({ children }) => {
     return (
         <div className="layout-container">
-            <head>
-                <link rel="preload" href="../static/fonts/Poppins-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-                <link rel="preload" href="../static/fonts/Poppins-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-                <link rel="preload" href="../static/fonts/Poppins-Light.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-                <link rel="preload" href="../static/fonts/Poppins-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-                <link rel="preload" href="../static/fonts/Poppins-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-            </head>
             <Header />
             {children}
             <Footer />
